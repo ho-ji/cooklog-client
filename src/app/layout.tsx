6b0641214a25376577196750657e0fa216fe1333
ho-ji <wkdd0205@gmail.com>
2024-08-19 22:52:43 +0900

    feat: 초기 프로젝트 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type {Metadata} from 'next'
+import {Noto_Sans_KR} from 'next/font/google'
+import './globals.css'
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  preload: false,
+})
+
+export const metadata: Metadata = {
+  title: '쿡로그',
+  description: '요리 레시피 블로그 쿡로그',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html>
+      <body className={`${notoSansKR.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
