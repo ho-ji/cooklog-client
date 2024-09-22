0ae440875120f8765a975c3fea860c7f1a02fdf9
ho-ji <wkdd0205@gmail.com>
2024-09-22 16:38:17 +0900

    feat: 푸터 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer className="bg-gray-50 text-sm py-12 text-center text-gray-400">
+      <p>본 페이지는 상업적인 목적이 없는 포트폴리오용 사이트입니다</p>
+      <p>©2024 장예지. All rights reserved.</p>
+    </footer>
+  )
 }
 
 export default Footer
