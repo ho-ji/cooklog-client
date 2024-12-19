a7f3f0c18926bd4581924c8e4e2289a5ac153dde
ho-ji <wkdd0205@gmail.com>
2024-12-19 19:24:06 +0900

    feat: 비밀번호 UI 추가

--- src/components/signup/SignUpForm.tsx
+++ src/components/signup/SignUpForm.tsx
@@ -213,18 +213,29 @@ const SignUpForm = () => {
           </>
         )}
       </div>
-      <label>비밀번호</label>
-      <input
-        className="input"
-        value={password.value}
-        onChange={password.handleChange}
-      />
-      <label>비밀번호 확인</label>
-      <input
-        className="input"
-        value={confirmPassword.value}
-        onChange={confirmPassword.handleChange}
-      />
+      <div className="flex flex-col">
+        <label htmlFor="password">비밀번호</label>
+        <p className="text-xs mb-2 text-gray-500">영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.</p>
+        <input
+          className="input"
+          value={password.value}
+          onChange={password.handleChange}
+          type="password"
+          placeholder="비밀번호"
+          id="password"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="confirm-password">비밀번호 확인</label>
+        <input
+          className="input"
+          value={confirmPassword.value}
+          onChange={confirmPassword.handleChange}
+          type="password"
+          placeholder="비밀번호 확인"
+          id="confirm-password"
+        />
+      </div>
       <button className="button-primary">회원가입</button>
     </form>
   )
