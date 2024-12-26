346fae39fc38620764dc7cb332249c56c451015b
ho-ji <wkdd0205@gmail.com>
2024-12-26 23:11:37 +0900

    refactor: 비밀번호 정규식 수정

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -4,6 +4,6 @@ const isValidDomain = (domain: string): boolean => /^[a-zA-Z0-9.-]+\.[a-zA-z]{2,
 
 export const emailValidator = (emailId: string, domain: string): boolean => isValidEmailId(emailId) && isValidDomain(domain)
 
-export const passwordValidator = (password: string): boolean => /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+==])(?=.*[0-9]).{8,16}$/.test(password)
+export const passwordValidator = (password: string): boolean => /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\d@$!%*?&#.~_-]{8,16}$/.test(password)
 
 export const nicknameValidator = (nickname: string): boolean => /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,20}$/.test(nickname)
