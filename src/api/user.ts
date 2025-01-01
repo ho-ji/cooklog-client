1d5954fdd2a179e60cae48ca20fa1e5554099f16
ho-ji <wkdd0205@gmail.com>
2025-01-01 14:11:59 +0900

    refactor: API 반환값 수정으로 인한 수정 및 global.css 로딩스피너 mask 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,7 +3,7 @@ import {instance} from '.'
 export const verifyEmailAPI = async (email: string) => {
   try {
     const result = await instance.get(`/api/user/verify-email/${email}`)
-    return result.data
+    if (result.data.success) return result.data.data
   } catch (error) {
     throw error
   }
